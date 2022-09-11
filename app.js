9691f4a661f3f190bb3caec7089d15538b5e7989
puppy2111 <huynhdaihuybank3@gmail.com>
2022-09-11 18:53:59 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require('./app/api-error');
 const app = express();
 const contactRouter = require('./app/routes/contact.route');
+
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactRouter);
-
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 module.exports = app;
