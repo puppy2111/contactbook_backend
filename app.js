675b761f6ebf959cdaffd3d1396b5635ab301e30
puppy2111 <huynhdaihuybank3@gmail.com>
2022-09-11 18:47:45 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+const contactRouter = require('./app/routes/contact.route');
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact booke application." });
-});
+app.use('/api/contacts', contactRouter);
 
 module.exports = app;
